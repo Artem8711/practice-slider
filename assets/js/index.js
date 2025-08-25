dc18c99b22d89646daefcb15e06e0a2d2085d4cb
Artem8711 <goglil2063@gmail.com>
2025-08-25 11:21:32 +0200

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+const slider = [
+   {
+      src: `https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg`,
+      alt: `landscape1`
+   },
+   {
+      src: `https://images.unsplash.com/photo-1506744038136-46273834b3fb`,
+      alt: `landscape2`
+   },
+   {
+      src: `https://cdn.pixabay.com/photo/2015/01/19/13/51/car-604019_1280.jpg`,
+      alt: `landscape3`
+   },
+   {
+      src: `https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/NASA-Hubble-Space-Telescope-image-of-the-Pillars-of-Creation.jpg/800px-NASA-Hubble-Space-Telescope-image-of-the-Pillars-of-Creation.jpg`,
+      alt: `landscape4`
+   }
+];
