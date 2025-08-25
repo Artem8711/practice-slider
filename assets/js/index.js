93ab787079d692e9ba479be5a224d8fc154b0870
Artem8711 <goglil2063@gmail.com>
2025-08-25 13:20:18 +0200

    Удалить слайдер и добавить счетчик с кнопками увеличения и уменьшения

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,19 @@
-const slider = [
-   {
-      src: `https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg`,
-      alt: `landscape1`
-   },
-   {
-      src: `https://images.unsplash.com/photo-1506744038136-46273834b3fb`,
-      alt: `landscape2`
-   },
-   {
-      src: `https://cdn.pixabay.com/photo/2015/01/19/13/51/car-604019_1280.jpg`,
-      alt: `landscape3`
-   },
-   {
-      src: `https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/NASA-Hubble-Space-Telescope-image-of-the-Pillars-of-Creation.jpg/800px-NASA-Hubble-Space-Telescope-image-of-the-Pillars-of-Creation.jpg`,
-      alt: `landscape4`
-   }
-];
+const [decBtn, incBtn] = document.querySelectorAll('.counterBtn');
+const counterEl = document.querySelector('#counter');
 
-let currentSlideIndex = 0;
+let counter = 0;
+counterEl.textContent = counter;
 
-const slideImg = document.querySelector(`img`);
-
-slideImg.src = slider[0].src;
-slideImg.alt = slider[0].alt;
-
-//////////////////////////////
-
-const [prevBtn, nextBtn] = document.querySelectorAll(`.navBtn`);
-
-function nextSlideHandler() {
-   if (currentSlideIndex < slider.length - 1) {
-      currentSlideIndex++;
-   } else {
-      currentSlideIndex = 0;
-   }
-
-   slideImg.src = slider[currentSlideIndex].src;
-   slideImg.alt = slider[currentSlideIndex].alt;
+function decCount(e) {
+   counter--;
+   counterEl.textContent = counter;
 }
+decBtn.addEventListener('click', decCount);
 
-nextBtn.addEventListener(`click`, nextSlideHandler);
+//////////////////////////
 
-function prevSlideHandler() {
-   if (currentSlideIndex > 0) {
-      currentSlideIndex--;
-   } else {
-      currentSlideIndex = slider.length - 1;
-   }
-
-   slideImg.src = slider[currentSlideIndex].src;
-   slideImg.alt = slider[currentSlideIndex].alt;
+function incCount(e) {
+   counter++;
+   counterEl.textContent = counter;
 }
-prevBtn.addEventListener(`click`, prevSlideHandler);
+incBtn.addEventListener('click', incCount);
